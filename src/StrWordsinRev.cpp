e577fe609a2d06ae60703578415c486368c47303
rashmith222 <rashmith222@missionrnd.com>
2016-03-19 02:15:34 +0530

    Submit Commit

--- src/StrWordsinRev.cpp
+++ src/StrWordsinRev.cpp
@@ -13,29 +13,35 @@ NOTES: Don't create new string.
 #include<malloc.h>
 
 void str_words_in_rev(char *input, int len){
-	int i = 0, j = 0, t = 0, p = 0, k = 0, l = 0;
+	int i = 0, j = 0, t = 0, count = 0, p = 0, k = 0, l = 0;
 	char **a;
 	char b[10];
-	a = (char **)malloc(sizeof(char *) * 10);
+	a = (char **)malloc(10 * sizeof(char *));
 	for (l = 0; l<10; l++)
-		a[l] = (char *)malloc( len * sizeof(char));
+		a[l] = (char *)malloc(len*sizeof(char));
 	while (input[i] != '\0')
 	{
-		while (input[j] != ' ')
+		k = 0;
+		while (input[i] != ' ')
 		{
-			a[p][k] = input[j];
+			a[p][k] = input[i];
 			k++;
-			j++;
+			if (k > len)
+				break;
 			i++;
+			if (i>len){
+				break;
+			}
 		}
 		a[p][k] = '\0';
-		k = 0;
-		++p;
-		b[t] = input[j];
+		b[t] = input[i];
+		++count;
 		t++;
-		++j;
+		++p;
+		if (input[i] == ' ')
+			i++;
 	}
-	j = 0; p = p - 2; k = 0; t = 0;
+	j = 0; p = p - 1; k = 0; t = 0;
 	while (a[p] != '\0')
 	{
 		while (a[p][k] != '\0')
@@ -54,5 +60,3 @@ void str_words_in_rev(char *input, int len){
 	}
 	input[j] = '\0';
 }
-
-
